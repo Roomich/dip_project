faaa836c2c19226d5c14e5c211f42faf2d24a6a1
Roman Odinichenko <roomic13@gmail.com>
2023-04-25 12:53:29 +0300

    add test to app

---
+++ application/app/test_start.py
@@ -0,0 +1,11 @@
+import pytest
+from main import send_welcome
+from unittest.mock import AsyncMock
+
+
+@pytest.mark.asyncio
+async def test_start_command():
+    message = AsyncMock()
+    await send_welcome(message)
+
+    message.answer.assert_called_with("Привет!\nЯ тестовый бот\nОтправь мне любое сообщение, а я тебе обязательно отвечу.")
