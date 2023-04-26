0f61bddb0f253a1266e13c5a58103ec3c19b7417
Roman Odinichenko <roomic13@gmail.com>
2023-04-26 07:22:25 +0300

    asd

---
+++ jenkins/casc_configs/seedjob.groovy
@@ -0,0 +1,21 @@
+folder('build') {
+    description('<div style="border-radius:10px; text-align: center; font-size:120%; padding:15px; background-color: powderblue;">Build test deliver</div>')
+}
+
+pipelineJob('build/build_app') {
+    description('<div style="border-radius:10px; text-align: center; font-size:120%; padding:15px; background-color: powderblue;">Build-test-deliver pipeline</div>')
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        github('Roomich/dip_project', 'ssh')
+                        credentials('github_ssh')
+                    }
+                    branch('test')
+                }
+            }
+            scriptPath('jenkinsfile-app')
+        }
+    }
+}
